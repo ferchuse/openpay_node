2c28a95deca037e30330e4d5b6b2edf5ec1c94a8
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-10-31 13:06:42 -0600

    Add structure to project, add mysql connection, add env variables and basic routes to CUSTOMER

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # openpay_node
  
+## Basic routes
+
+List customers
+```javascript
+GET: api/v1/customers  // SELECT * FROM customers_table
+```
+
+Create new customer
+```javascript
+POST: api/v1/customer  // INSERT INTO customers_table
+```
--- index.js
+++ index.js
@@ -1,73 +1,11 @@
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const express = require('express');
+const app = require('./app');
+const connection = require('./connection');
 
-const app = express();
+connection.connect((err) => {
+	if (err) console.log(`Error al conectar: ${err.stack}`);
+	app.listen(3000, () => {
+		console.log("Corriendo Servidor");
+	});
 
-var Openpay = require('openpay');
-
-var openpay = new Openpay('m9wcsjwhqplsq01chivl', 'sk_bc7eb07a71574342954b94de857a84a9');
-
-
-var newCustomer = {
-  "name":"John",
-  "email":"johndoe@example.com",
-  "last_name":"Doe",
-  "address":{
-    "city":"Queretaro",
-    "state":"Queretaro",
-    "line1":"Calle Morelos no 10",
-    "line2":"col. san pablo",
-    "postal_code":"76000",
-    "country_code":"MX"
-	},
-  "phone_number":"44209087654"
-};
-
-
-app.use(bodyParser.urlencoded({}));
-app.use(bodyParser.json());
-app.use(cors());
-
-
-
-app.post('/', (req, res) => {
-	
-	
-	let params = req.body;
-	// let {} = ;
-	
-	
-	console.log("Params", params);
-	
-	// openpay.customers.create(newCustomer, function(error, body) {
-    // error;    // null if no error occurred (status code != 200||201||204)
-    // body;     // contains the object returned if no error occurred (status code 
-		// console.log("Body", body );
-		// console.log("Error", error );
-		res.send({"params": params});
-	// });
-	
 });
 
-// app.get('/', (req, resp) => {
-	
-	
-	// console.log("Get route /");
-	
-	// openpay.customers.create(newCustomer, function(error, body) {
-    // error;    // null if no error occurred (status code != 200||201||204)
-    // body;     // contains the object returned if no error occurred (status code == 200||201||204)
-		// console.log("Customer", newCustomer );
-		// console.log("Body", body );
-		// console.log("error", error );
-		
-	// });
-	
-// });
-
-
-app.listen(3000, () => {
-	
-	console.log("Corriendo Servidor");
-});
